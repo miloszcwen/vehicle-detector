e44c8a9d2a65a660431c2bcfa83ae4078ceaf1e4
miloszcwen <cwen.milosz@gmail.com>
2020-12-23 14:53:41 +0100

    refactor register to class component

--- src/components/register/Register.js
+++ src/components/register/Register.js
@@ -1,7 +1,37 @@
 import React from 'react';
 import './Register.css'
 
-const Register=({onRouteChange})=>{
+class Register extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            signInEmail: '',
+            signInPassword: ''
+        }
+    }
+    onEmailChange = (event)=>{
+        this.setState({signInEmail: event.target.value})
+    }
+    onPasswordChange = (event)=>{
+        this.setState({signInPassword: event.target.value})
+    }
+    onSubmitSignIn = ()=>{
+        fetch('http://localhost:3000/signin', {
+            method: 'post',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                email: this.state.signInEmail,
+                password: this.state.signInPassword
+            })
+        }).then(response=>response.json())
+        .then(data=>{
+            if (data === 'success'){
+                this.props.onRouteChange("home");
+            }
+        })
+    }
+    render() {
+        const {onRouteChange}= this.props;
     return (
         <article className="br2 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-2 center">
             <main className="pa4 ">
@@ -34,6 +64,7 @@ const Register=({onRouteChange})=>{
             </main>
         </article>
     )
+    }
 }
 
 export default Register;
