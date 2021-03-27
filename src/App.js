685de43f706f7dae0afeb9aafbec80337cb74f41
miloszcwen <cwen.milosz@gmail.com>
2021-03-27 16:45:12 +0100

    refactor Register to use hooks

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import './App.css';
-import Particles from 'react-particles-js';
-import React, {Component} from 'react';
-import Navigation from './components/navigation/Navigation';
-import Logo from './components/logo/Logo';
-import Rank from './components/rank/Rank';
-import ImageRecognition from './components/imageRecognition/ImageRecognition';
-import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
-import SignIn from './components/signIn/SignIn';
-import Register from './components/register/Register';
-
-
+import "./App.css";
+import Particles from "react-particles-js";
+import React, { Component } from "react";
+import Navigation from "./components/navigation/Navigation";
+import Logo from "./components/logo/Logo";
+import Rank from "./components/rank/Rank";
+import ImageRecognition from "./components/imageRecognition/ImageRecognition";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import SignIn from "./components/signIn/SignIn";
+import Register from "./components/register/Register";
 
 const initialState = {
   input: "",
@@ -22,12 +20,12 @@ const initialState = {
     name: "",
     email: "",
     carCounter: 0,
-    joined: ""
-  }
-}
+    joined: "",
+  },
+};
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       input: "",
@@ -40,123 +38,132 @@ class App extends Component {
         name: "",
         email: "",
         carCounter: 0,
-        joined: ""
-      }
-    }
+        joined: "",
+      },
+    };
   }
 
   loadUser = (data) => {
-    this.setState({user: {
-      id: data.id,
-      name: data.name,
-      email: data.email,
-      carCounter: data.carCounter,
-      joined: data.joined
-    }});
-  }
+    this.setState({
+      user: {
+        id: data.id,
+        name: data.name,
+        email: data.email,
+        carCounter: data.carCounter,
+        joined: data.joined,
+      },
+    });
+  };
 
   calculateCarLocation = (data) => {
-    const clarifaiCar = data.outputs[0].data.regions[0].region_info.bounding_box;
-    const image = document.getElementById('inputImage');
+    const clarifaiCar =
+      data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById("inputImage");
     const width = Number(image.width);
     const height = Number(image.height);
     return {
       leftCol: clarifaiCar.left_col * width,
       topRow: clarifaiCar.top_row * height,
-      rightCol: width - (clarifaiCar.right_col * width),
-      bottomRow: height - (clarifaiCar.bottom_row * height)
-    }
-  }
+      rightCol: width - clarifaiCar.right_col * width,
+      bottomRow: height - clarifaiCar.bottom_row * height,
+    };
+  };
 
-  displayCarBox = (box)=>{
-    this.setState({box: box});
-  }
+  displayCarBox = (box) => {
+    this.setState({ box: box });
+  };
 
-  onInputChange = (e)=>{
-    this.setState({input: e.target.value});
-  }
+  onInputChange = (e) => {
+    this.setState({ input: e.target.value });
+  };
 
-    onPictureSubmit = ()=>{
-      if(this.state.input){
-        this.setState({imgUrl: this.state.input});
-        fetch('https://protected-taiga-19734.herokuapp.com/imageurl', {
-              method: 'post',
-              headers: {'Content-Type': 'application/json'},
+  onPictureSubmit = () => {
+    if (this.state.input) {
+      this.setState({ imgUrl: this.state.input });
+      fetch("https://protected-taiga-19734.herokuapp.com/imageurl", {
+        method: "post",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          input: this.state.input,
+        }),
+      })
+        .then((response) => response.json())
+        .then((response) => {
+          if (response) {
+            fetch("https://protected-taiga-19734.herokuapp.com/image", {
+              method: "put",
+              headers: { "Content-Type": "application/json" },
               body: JSON.stringify({
-                 input: this.state.input
+                id: this.state.user.id,
+              }),
+            })
+              .then((response) => response.json())
+              .then((count) => {
+                this.setState(
+                  Object.assign(this.state.user, { carCounter: count })
+                );
               })
-           })
-           .then(response=>response.json())
-        .then(response=>{
-          if (response){
-            fetch('https://protected-taiga-19734.herokuapp.com/image', {
-              method: 'put',
-              headers: {'Content-Type': 'application/json'},
-              body: JSON.stringify({
-                 id: this.state.user.id
-              })
-           })
-           .then(response=>response.json())
-           .then(count=>{
-              this.setState(Object.assign(this.state.user,{carCounter: count}))
-          })
-          .catch(error=>{console.log(error)});
+              .catch((error) => {
+                console.log(error);
+              });
           }
-          this.displayCarBox(this.calculateCarLocation(response))
+          this.displayCarBox(this.calculateCarLocation(response));
         })
-        .catch(err=>console.log(err))
-      }
-  }
+        .catch((err) => console.log(err));
+    }
+  };
 
-  onRouteChange = (route)=>{
-    if(route==="home"){
-      this.setState({isSignedIn: true});
-    } else if(route==="signin"){
+  onRouteChange = (route) => {
+    if (route === "home") {
+      this.setState({ isSignedIn: true });
+    } else if (route === "signin") {
       this.setState(initialState);
     }
-    this.setState({route: route})
-  }
+    this.setState({ route: route });
+  };
 
   render() {
-    const {imgUrl, isSignedIn, box, route} = this.state;
+    const { imgUrl, isSignedIn, box, route } = this.state;
     return (
       <>
-    <div className="App">
-      <Particles className="particles" />
-      <Navigation onRouteChange={this.onRouteChange} isSignedIn={isSignedIn}/>
-      { route === "home"
-      ? <> <Logo />
-      <Rank
-        userName={this.state.user.name}
-        carCounter={this.state.user.carCounter}
-      />
-      <ImageLinkForm
-        onInputChange={this.onInputChange}
-        onButtonSubmit={this.onPictureSubmit}
-      />
-      <ImageRecognition
-        imgUrl={imgUrl}
-        box={box}
-      />
+        <div className="App">
+          <Particles className="particles" />
+          <Navigation
+            onRouteChange={this.onRouteChange}
+            isSignedIn={isSignedIn}
+          />
+          {route === "home" ? (
+            <>
+              {" "}
+              <Logo />
+              <Rank
+                userName={this.state.user.name}
+                carCounter={this.state.user.carCounter}
+              />
+              <ImageLinkForm
+                onInputChange={this.onInputChange}
+                onButtonSubmit={this.onPictureSubmit}
+              />
+              <ImageRecognition imgUrl={imgUrl} box={box} />
+            </>
+          ) : route === "signin" ? (
+            <SignIn
+              loadUser={this.loadUser}
+              onRouteChange={this.onRouteChange}
+            />
+          ) : (
+            <Register
+              loadUser={this.loadUser}
+              onRouteChange={this.onRouteChange}
+            />
+          )}
+        </div>
       </>
-      : (route === "signin"
-      ? <SignIn
-        loadUser={this.loadUser}
-        onRouteChange={this.onRouteChange}
-        />
-      : <Register
-        loadUser={this.loadUser}
-        onRouteChange={this.onRouteChange}
-        />
-      )}
-    </div>
-    </>
-    )
+    );
   }
 }
 
 export default App;
-
 
 // for api testing:
 // test images:
@@ -176,11 +183,11 @@ export default App;
 // aa7f35c01e0642fda5cf400f543e7c40 - general
 // f950c58836faebadfdd44d709fb7cef6 - vehicle
 
-    // app.models.initModel({id: Clarifai.GENERAL_MODEL, version: "aa7f35c01e0642fda5cf400f543e7c40"})
-    //   .then(generalModel => {
-    //     return generalModel.predict(url);
-    //   })
-    //   .then(response => {
-    //     var concepts = response['outputs'][0]['data']['concepts']
-    //     console.log(concepts)
-    //   })
+// app.models.initModel({id: Clarifai.GENERAL_MODEL, version: "aa7f35c01e0642fda5cf400f543e7c40"})
+//   .then(generalModel => {
+//     return generalModel.predict(url);
+//   })
+//   .then(response => {
+//     var concepts = response['outputs'][0]['data']['concepts']
+//     console.log(concepts)
+//   })
